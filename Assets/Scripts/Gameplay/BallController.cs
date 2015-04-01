33d47a52d214adff8fd26398f601b77a9a040651
Alexander Guk <alex.guk.kpi@gmail.com>
2015-04-01 21:46:15 +0300

    Added ball acceleration with blast wave

--- Assets/Scripts/Gameplay/BallController.cs
+++ Assets/Scripts/Gameplay/BallController.cs
@@ -6,6 +6,7 @@ public class BallController : MonoBehaviour {
     public float kinematicTime;
     public float accelerationTime;
     public float scoreLineMargin;
+	public float bumpAccelerationCoeff;
 
     private float initialDirection;
 	private float currentKinematicTime;
@@ -37,10 +38,12 @@ public class BallController : MonoBehaviour {
 			currentSpeed += Time.fixedDeltaTime / accelerationTime * (maxSpeed - minSpeed);
 			float currentAcceleration = (currentSpeed - minSpeed) / (maxSpeed - minSpeed);
 			if (currentAcceleration < 0.5) {
-				updateColor(Color.red, Time.fixedDeltaTime / (accelerationTime / 2));
+				updateColor(Color.black, Color.red, currentAcceleration * 2f);
 			} else {
-				updateColor(Color.yellow, Time.fixedDeltaTime / (accelerationTime / 2));
+				updateColor(Color.red, Color.yellow, (currentAcceleration - 0.5f) * 2f);
 			}
+		} else {
+			spriteRenderer.color = Color.yellow;
 		}
 		
 		GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity.normalized * currentSpeed;
@@ -76,20 +79,20 @@ public class BallController : MonoBehaviour {
 		}
 	}
 	
-	private void updateColor(Color newColor, float step)
-	{
-		Color colorDelta = new Color((newColor.r - spriteRenderer.color.r) * step,
-		                             (newColor.g - spriteRenderer.color.g) * step,
-		                             (newColor.b - spriteRenderer.color.b) * step);
-		spriteRenderer.color += colorDelta;
+	private void updateColor(Color previousColor, Color newColor, float currentAcceleration) {
+		spriteRenderer.color = new Color(previousColor.r + (newColor.r - previousColor.r) * currentAcceleration,
+		                                 previousColor.g + (newColor.g - previousColor.g) * currentAcceleration,
+		                                 previousColor.b + (newColor.b - previousColor.b) * currentAcceleration);
 	}
 
 	void OnCollisionEnter2D(Collision2D collision) {
         AudioController.Instance.collisionBallSound.Play();
 	}
 
-	public void SetDirection(float direction) {
+	public void Bump(float direction) {
 		GetComponent<Rigidbody2D>().velocity = 
 			new Vector2(Mathf.Cos(direction) * currentSpeed, Mathf.Sin(direction) * currentSpeed);
+		currentSpeed += (maxSpeed - minSpeed) * bumpAccelerationCoeff;
+		currentSpeed = Mathf.Clamp(currentSpeed, minSpeed, maxSpeed);
 	}
 }
--- Assets/Scripts/Gameplay/BlastWaveController.cs
+++ Assets/Scripts/Gameplay/BlastWaveController.cs
@@ -6,7 +6,7 @@ public class BlastWaveController : MonoBehaviour {
 	void Start() {
 		foreach (GameObject go in GameObject.FindGameObjectsWithTag("Ball")) {
 			if (Vector2.Distance(transform.position, go.transform.position) < transform.parent.gameObject.GetComponent<SpriteRenderer>().bounds.size.x / 2f * effectRangeCoeff) {
-				go.GetComponent<BallController>().SetDirection(Mathf.Atan2(go.transform.position.y - transform.position.y,
+				go.GetComponent<BallController>().Bump(Mathf.Atan2(go.transform.position.y - transform.position.y,
 				                                                           go.transform.position.x - transform.position.x));
 			}
 		}
